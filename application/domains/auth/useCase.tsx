00136131d560d0b41901ebd4ffa20cfda87e04fb
Weronika Jankowska <wjankowska8@gmail.com>
2024-06-05 13:34:51 +0200

    🤡 Add mocked '/login' API route

---
+++ application/domains/auth/useCase.tsx
@@ -0,0 +1,10 @@
+export const createUseCaseAuth = () => {
+  const login = async (email: string, password: string) => {
+    console.log("login", email, password)
+    return
+  } 
+
+  return (
+    login
+  )
+}
